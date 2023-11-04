92605aa6ed41f8084ad414fd7827d5055adaaebb
crude-dreams <130430056+crude-dreams@users.noreply.github.com>
2023-11-04 15:20:19 +0530

    Add files via upload

---
+++ Calculator/assets/script.js
@@ -0,0 +1,33 @@
+var input= document.getElementById("inputbox");
+var button= document.querySelectorAll("button");
+
+var string="";
+var arr= Array.from(button);
+arr.forEach(button => 
+    {
+        button.addEventListener("click",(e) =>
+        {
+            if(e.target.innerHTML=="=")
+            {
+                string=eval(string);
+                input.value=string;
+            }
+            else if(e.target.innerHTML=="C")
+            {
+                string=string.substring(0,string.length-1);
+                input.value=string;
+            }
+            else if(e.target.innerHTML=="AC")
+            {
+                string="";
+                input.value=string;
+            }
+            else
+            {
+                string+=e.target.innerHTML;
+                input.value=string;
+            }
+        }
+        )
+    }
+)
